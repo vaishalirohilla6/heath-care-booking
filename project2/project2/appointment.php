bd73ff6a10c61fc2485a26e791d0929a30e37e05
vaishalirohilla6 <95029696+vaishalirohilla6@users.noreply.github.com>
2022-04-22 21:43:11 +0530

    Add files via upload

---
+++ project2/project2/appointment.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>book appointment</title>
+    <link rel="stylesheet" href="appoint.css">
+</head>
+<body>
+
+    <div class="banner"></div>
+
+    <div class="form">
+        <form action="#">
+            <h2 class="heading">Appointment Form</h2>
+            
+                 <h3 class="name"> Your Name</h3>
+                 <input type="text" class="Name" name"" placeholder=" Your Name">
+                 <h3 class="add"> Your Address</h3>
+                 <textarea class="Add"></textarea>
+                 <h3 class="num">Contact Number </h3>
+                 <input type="text" class="number" placeholder="contact number" name"">
+                 <button class="btn2">SEND OTP</button>
+                 <input maxlength="4" type="password"class="otp" value="">
+                 <h3 class="date">Date </h3>
+                 <input type="date" class="Date" value="" >
+                 <h3 class="time">Time </h3>
+                 <select class="am">
+                    <option>AM</option>
+                    <option>PM</option>
+                   </select>
+                 <input type="time" class="Time" value="" >
+                 <button class="btn">Book Appointment</button>
+                    
+  </form>
+       </div>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
